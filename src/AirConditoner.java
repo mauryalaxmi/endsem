4f8f346495621a16d2784bb779e03b5fa165f92e
laxmi <laxshu1516@gmail.com>
2017-06-08 11:57:28 +0530

    commit3

--- src/AirConditoner.java
+++ src/AirConditoner.java
@@ -1,6 +1,10 @@
 
 public class AirConditoner {
 	public int flag=0;
+	private int temp;
+	public AirConditoner(int temp) {
+		this.temp=temp;
+	}
 	public  void On()
 	{
 		if(this.flag==0)
@@ -15,5 +19,26 @@ public class AirConditoner {
 			System.out.println("the ac are off");
 			this.flag=0;
 		}
+	}
+	public void tempup(){
+		On();
+		if(flag==1)
+		{
+			this.setTemp(this.getTemp() + 1);
+			
+		}
+	}
+	public void tempdown(){
+		On();
+		if(flag==1)
+		{
+			this.setTemp(this.getTemp() - 1);	
+		}
+	}
+	public int getTemp() {
+		return temp;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
 	}
 }
---
+++ src/AirConditonerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AirConditonerTest {
+	AirConditoner ac1;
+	@Before
+	public void setUp() throws Exception {
+		ac1 = new AirConditoner(15);
+		
+	}
+
+	@Test
+	public void testTempup() {
+		//fail("Not yet implemented");
+		ac1.tempup();
+		assertEquals(16,ac1.getTemp());
+	}
+
+	@Test
+	public void testTempdown() {
+		//fail("Not yet implemented");
+		ac1.tempdown();
+		assertEquals(14,ac1.getTemp());
+	}
+
+}
